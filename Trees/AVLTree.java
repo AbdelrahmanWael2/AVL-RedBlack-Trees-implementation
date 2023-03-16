a6f98924751497f1167fa7585eae4151b286ef2c
Mohamed <mohamedraffeek2001@gmail.com>
2023-03-16 17:46:12 +0200

    Added left and right rotate methods

--- Trees/AVLTree.java
+++ Trees/AVLTree.java
@@ -33,6 +33,41 @@ public class AVLTree<K extends Comparable<K>> implements ITree<K> {
         return "The tree height is" + this.height + "\n";
     }
 
+    private int max(int leftHeight, int rightHeight) {
+        if(leftHeight >= rightHeight) return leftHeight;
+        else return rightHeight;
+    }
+
+    private void rightRotate(AVLNode<K> currentNode){
+        AVLNode<K> temp1 = currentNode.getLeft();
+        AVLNode<K> temp2 = temp1.getRight();
+        AVLNode<K> temp3 = currentNode.getParent();
+        // rotate around currentNode
+        temp1.setRight(currentNode);
+        currentNode.setParent(temp1);
+        temp1.setParent(temp3);
+        currentNode.setLeft(temp2);
+        temp2.setParent(currentNode);
+        //update heights for currentNode and temp1
+        currentNode.setLeftHeight(max(temp2.getLeftHeight(), temp2.getRightHeight()));
+        temp1.setRightHeight(max(currentNode.getLeftHeight(), currentNode.getRightHeight()));
+    }
+
+    private void leftRotate(AVLNode<K> currentNode){
+        AVLNode<K> temp1 = currentNode.getRight();
+        AVLNode<K> temp2 = temp1.getLeft();
+        AVLNode<K> temp3 = currentNode.getParent();
+        // rotate around currentNode
+        temp1.setLeft(currentNode);
+        currentNode.setParent(temp1);
+        temp1.setParent(temp3);
+        currentNode.setRight(temp2);
+        temp2.setParent(currentNode);
+        //update heights for currentNode and temp1
+        currentNode.setRightHeight(max(temp2.getLeftHeight(), temp2.getRightHeight()));
+        temp1.setLeftHeight(max(currentNode.getLeftHeight(), currentNode.getRightHeight()));
+    }
+
     private AVLNode<K> searchRecursion(AVLNode<K> currentNode, K value) {
         int found = currentNode.getKey().compareTo(value);
         if (found < 0) {
